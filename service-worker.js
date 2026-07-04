fedc90f2ec38eeb2af563569c1a2ecdd6d60c393
jeldjor <6nbzd8pykj@privaterelay.appleid.com>
2026-07-04 12:34:09 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-// cache v2.70
-const CACHE='kim-planner-v268';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./logo.png']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
