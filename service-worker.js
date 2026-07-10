ee1db04666393f945cd4f9e3b8239bed46c23873
jeldjor <6nbzd8pykj@privaterelay.appleid.com>
2026-07-10 21:32:54 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-// mobile fix 2026-07-10
-const CACHE='planning-v8-admin-clear-fix';
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{});
